08858cd45957ab14ce125704cdf97690691d06cb
Julian Pierri <estudios.w6qo3@silomails.com>
2025-10-20 13:38:09 -0700

    Nombre de usuario se muestra en navbar

--- Libreria/MasterPage.Master.cs
+++ Libreria/MasterPage.Master.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 
 namespace Libreria
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
+        public string UserName { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["usuario"] != null)
+            {
+                var dataCli = new AccesoClientes();
+                Usuario user = (Usuario)Session["usuario"];
+                switch (user.TipoUsuario)
+                {
+                    // admin
+                    default:
+                        UserName = "Admin";
+                        break;
+                    // Cliente
+                    case TipoUsuario.Cliente:
+                        UserName = dataCli.Listar().Find(x => x.Usuario.IdUsuario == user.IdUsuario).Nombre;
+                        break;
+                }
+            }
         }
         protected void btnInicio_click(object sender, EventArgs e)
         {
             Response.Redirect("Default.aspx");
         }
-        protected void btnAccount_click (object sender, EventArgs e)
+        protected void btnCuenta_click (object sender, EventArgs e)
         {
             Response.Redirect("Cuenta.aspx");
         }
@@ -24,10 +42,11 @@ namespace Libreria
         {
             Response.Redirect("Carrito.aspx");
         }
-        protected void btnCuenta_click(object sender, EventArgs e) 
+        protected void btnContacto_click(object sender, EventArgs e) 
         {
             Response.Redirect("Contacto.aspx");
         }
+
         protected void btnLogout_click(object sender, EventArgs e)
         {
             Session.Clear();
--- Libreria/MasterPage.Master.designer.cs
+++ Libreria/MasterPage.Master.designer.cs
@@ -42,13 +42,13 @@ namespace Libreria
         protected global::System.Web.UI.WebControls.LinkButton btnHome;
 
         /// <summary>
-        /// btnAccount control.
+        /// btnCuenta control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnAccount;
+        protected global::System.Web.UI.WebControls.LinkButton btnCuenta;
 
         /// <summary>
         /// btnDeseados control.
