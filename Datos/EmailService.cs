cff3243467101fcee2e1e1564ad542340919a4e1
Julian Pierri <estudios.w6qo3@silomails.com>
2025-11-27 18:23:50 -0300

    Nuevo mail sender
    El anterior fue bloqueado por Google :/

--- Datos/EmailService.cs
+++ Datos/EmailService.cs
@@ -13,7 +13,7 @@ namespace Dominio
         {
             server = new SmtpClient
             {
-                Credentials = new NetworkCredential("libreriautnfrgp@gmail.com", "devn vtvn ehnn dysv"), // Clave de Google App Libreria UTN FRGP
+                Credentials = new NetworkCredential("utnfrgplibreria@gmail.com", "vvpt zzcf ytpb reuf"), // Clave de Google App Libreria UTN FRGP
                 EnableSsl = true,
                 Port = 587,
                 Host = "smtp.gmail.com"
