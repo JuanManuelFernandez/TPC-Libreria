a4febec5a8181826decdc4cacae6a265e757a906
Julian Pierri <estudios.w6qo3@silomails.com>
2025-12-10 17:31:48 -0300

    Quitado clase Stocks
    Era redundante dado que Libros ya tiene su valor de Cantidad

--- Datos/Stock.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dominio
-{
-    public class Stock
-    {
-        public int IdStock { get; set; }
-        public int IdLibro { get; set; }
-        public int Cantidad { get; set; }
-    }
-}
